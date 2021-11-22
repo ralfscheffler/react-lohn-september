f5a147dd02607a5587ea590d46f5c26a84ecefd1
ralfscheffler <scheffler@scheffler-hardcore.net>
2021-11-22 16:22:59 +0100

    Sonntagszuschlag gefixt

--- src/components/SonntagsZuschlag.js
+++ src/components/SonntagsZuschlag.js
@@ -1,21 +1,19 @@
 import { endOfDay, isSunday, parseISO } from "date-fns";
-
+import IsNextDay from "./IsNextDay";
 
 const SonntagsZuschlag = (start, end) => {
-    const todaySonntag = isSunday(parseISO(start));
-    const nextDaySonntag = isSunday(parseISO(end));
-    if (todaySonntag && !nextDaySonntag) {
-        return  parseFloat(
-          (Math.abs(end - start) / 3600000).toFixed(2)
-        );
-      }
-      if (!todaySonntag && nextDaySonntag) {
-        return parseFloat(
-          (
-            Math.abs(endOfDay(parseISO(start)) - end) / 3600000
-          ).toFixed(2)
-        );
-      }
-}
- 
+  const todaySonntag = isSunday(parseISO(start));
+  const nextDaySonntag = IsNextDay(start, end);
+
+  start = new Date(start);
+  end = new Date(end);
+
+  if (todaySonntag && !nextDaySonntag) {
+    return parseFloat((Math.abs(end - start) / 3600000).toFixed(2));
+  }
+  if (!todaySonntag && nextDaySonntag) {
+    return parseFloat((Math.abs(endOfDay(start) - end) / 3600000).toFixed(2));
+  }
+};
+
 export default SonntagsZuschlag;
--- src/components/UpdateShiftPlan.js
+++ src/components/UpdateShiftPlan.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const UpdateShiftPlan = async (changes) => {
-let returnCode= []
-  changes.map( async (element)=>{
+  changes.map(async (element) => {
     var id = parseInt(element.id);
-    var url=`http://scheffler-hardcore.de:2010/hardcore/dp/DP_T_Plan(${id})`;
+    var url = `http://scheffler-hardcore.de:2010/hardcore/dp/DP_T_Plan(${id})`;
     var data = await axios.patch(url, element);
     //return (console.log(data))
-    return returnCode.push(data.statusText)
-  })
-return (returnCode) 
+    return data;
+  });
+  //return returnCode;
 };
 
 export default UpdateShiftPlan;
